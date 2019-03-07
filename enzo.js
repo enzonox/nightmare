7b64c828c8b2782802bc45fefdb7f120cf6cd31d
enzonox <33371404+enzonox@users.noreply.github.com>
2019-03-07 14:07:52 -0300

    Add files via upload

---
+++ enzo.js
@@ -0,0 +1,21 @@
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true })
+
+nightmare
+  .goto('https://coinmarketcap.com/pt-br/')
+  //.click('a.currency-name-container.link-secondary')
+  //.click('a.price')
+  //.wait('#r1-0 a.result__a')
+  .evaluate(() => {
+        nome = document.querySelector('a').innerText;
+        moeda = document.querySelector('a.price').innerText;
+        vetor = [nome,moeda];
+          return vetor;
+      })
+  .end()
+  .then(result => { // ou (result) =>{
+    console.log(result);
+  })
+  .catch(error => {
+    console.error('Search failed:', error)
+  })
